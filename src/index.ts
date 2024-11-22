ab44d5813c6a64aef7bf92093a89212edf4ee6a2
Matt F <mwflaher@users.noreply.github.com>
2024-11-22 15:27:43 +0000

    feat(src/lib): adding a bunch of boilerplate sample stuff

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,32 @@
 import { MavenAGIClient } from 'mavenagi';
+import { EntityId } from 'mavenagi/api';
+
+import {
+  SAMPLE_ACTION_ID,
+  handleSampleTrigger,
+  refreshSampleKnowledge,
+  runSampleAction,
+  upsertSampleKnowledge,
+} from './lib';
 
 export default {
-  async preInstall({}: { organizationId: string; agentId: string; settings: AppSettings }) {
-    // Pre-install logic here
+  /**
+   * Use for any required validation steps, eg. credentials etc.
+   */
+  async preInstall({
+    organizationId,
+    agentId,
+  }: {
+    organizationId: string;
+    agentId: string;
+    settings: AppSettings;
+  }) {
+    console.info('preInstall', { organizationId, agentId });
   },
 
+  /**
+   * For installing actions, knowledge, etc
+   */
   async postInstall({
     organizationId,
     agentId,
@@ -13,18 +35,28 @@ export default {
     agentId: string;
     settings: AppSettings;
   }) {
-    new MavenAGIClient({
+    console.info('postInstall', { organizationId, agentId });
+
+    const client = new MavenAGIClient({
       organizationId: organizationId,
       agentId: agentId,
     });
 
-    // Setup actions, users, knowledge, etc
+    console.info(`Installed '${SAMPLE_ACTION_ID}' action`, {
+      organizationId,
+      agentId,
+    });
+
+    const title = 'United States Constitution';
+    const content = '123';
+    const slug = '123';
+
+    await upsertSampleKnowledge(client, { title, content, slug });
   },
 
-  async executeAction({}: { actionId: string; parameters: Record<string, string> }) {
-    // Execute action logic here
-  },
-
+  /**
+   * Handler for refresh of knowledge base
+   */
   async knowledgeBaseRefreshed({
     organizationId,
     agentId,
@@ -34,11 +66,59 @@ export default {
     knowledgeBaseId: { referenceId: string };
     settings: AppSettings;
   }) {
-    new MavenAGIClient({
+    console.info('knowledgeBaseRefreshed', { organizationId, agentId });
+
+    const client = new MavenAGIClient({
       organizationId: organizationId,
       agentId: agentId,
     });
 
-    // Setup actions, users, knowledge, etc
+    const title = 'United States Constitution';
+    const slug = 'us-constitution';
+    const response = await fetch('/constitution.md');
+    const content = await response.text();
+
+    await refreshSampleKnowledge(client, { title, content, slug });
+  },
+
+  /**
+   * Trigger on conversation creation and update
+   */
+  async conversationCreatedOrUpdated({
+    agentId,
+    organizationId,
+    conversations,
+  }: {
+    agentId: string;
+    organizationId: string;
+    conversations: EntityId[];
+    settings: AppSettings;
+  }) {
+    console.info('conversationCreatedOrUpdated', { organizationId, agentId });
+
+    const client = new MavenAGIClient({
+      organizationId: organizationId,
+      agentId: agentId,
+    });
+
+    await handleSampleTrigger(client, { conversations });
+  },
+
+  /**
+   * Handler for any installed action(s)
+   */
+  async executeAction({
+    actionId,
+    parameters,
+  }: {
+    actionId: string;
+    parameters: Record<string, string>;
+  }) {
+    console.info('executeAction', { actionId });
+
+    if (0 === SAMPLE_ACTION_ID.localeCompare(actionId)) {
+      const content = await runSampleAction({ parameters });
+      return content;
+    }
   },
 };
---
+++ src/lib/knowledge.ts
@@ -0,0 +1,67 @@
+import { MavenAGI, MavenAGIClient } from 'mavenagi';
+
+const SAMPLE_KNOWLEDGE_NAME = '';
+const SAMPLE_KNOWLEDGE_ID = '';
+
+export const upsertSampleKnowledge = async (
+  client: MavenAGIClient,
+  {
+    title,
+    content,
+    slug,
+  }: {
+    title: string;
+    content: string;
+    slug: string;
+  }
+) => {
+  try {
+    await client.knowledge.createOrUpdateKnowledgeBase({
+      name: SAMPLE_KNOWLEDGE_NAME,
+      type: MavenAGI.KnowledgeBaseType.Api,
+      knowledgeBaseId: { referenceId: SAMPLE_KNOWLEDGE_ID },
+    });
+
+    await client.knowledge.createKnowledgeBaseVersion(SAMPLE_KNOWLEDGE_ID, {
+      type: 'FULL',
+    });
+
+    await client.knowledge.createKnowledgeDocument(SAMPLE_KNOWLEDGE_ID, {
+      title,
+      content,
+      contentType: 'MARKDOWN',
+      knowledgeDocumentId: { referenceId: slug },
+    });
+
+    await client.knowledge.finalizeKnowledgeBaseVersion(SAMPLE_KNOWLEDGE_ID);
+  } catch (error) {
+    console.error('Error creating knowledge base', error);
+    throw error;
+  }
+};
+
+export const refreshSampleKnowledge = async (
+  client: MavenAGIClient,
+  {
+    title,
+    content,
+    slug,
+  }: {
+    title: string;
+    content: string;
+    slug: string;
+  }
+) => {
+  await client.knowledge.createKnowledgeBaseVersion(SAMPLE_KNOWLEDGE_ID, {
+    type: 'FULL',
+  });
+
+  await client.knowledge.createKnowledgeDocument(SAMPLE_KNOWLEDGE_ID, {
+    title,
+    content,
+    contentType: 'MARKDOWN',
+    knowledgeDocumentId: { referenceId: slug },
+  });
+
+  await client.knowledge.finalizeKnowledgeBaseVersion(SAMPLE_KNOWLEDGE_ID);
+};
