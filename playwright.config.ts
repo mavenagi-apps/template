743f2082616a2fd8fbd4282fcc19378bf7f77265
Matt Flaherty <mwflaher@alum.mit.edu>
2024-11-26 20:38:09 -0500

    fix(github): add start-server-and-test

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,7 +72,6 @@ export default defineConfig({
     // },
   ],
 
-  /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
   //   url: 'http://127.0.0.1:3000',
